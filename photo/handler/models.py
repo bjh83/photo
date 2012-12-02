fd3a440ceb77178e30f99e5061c85043e8e91a98
Brendan Higgins <bjh83@case.edu>
2012-12-02 06:41:45 -0500

    NF: I created the model and template for the photo page

--- photo/handler/models.py
+++ photo/handler/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Album(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __unicode__(self):
+		return self.name
+
+class Picture(models.Model):
+	album = models.ForeignKey(Album)
+	name = models.CharField(max_length=100)
+	image = models.ImageField(upload_to="pictures", max_length=200)
+
+	def __unicode__(self):
+		return self.name
+
